447c70aa7703f5ff6c575a76785a00fa9830f57a
Jake Benton <jake.benton@adp.com>
2018-03-13 14:08:21 -0400

    initial commit

---
+++ __tests__/appium.test.js
@@ -0,0 +1,32 @@
+import wd from 'wd';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+chai.should();
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+const PORT = 4723;
+const androidConfig = {
+  platformName: 'Android',
+  deviceName: 'Android Emulator',
+  app: './android/app/build/outputs/apk/app-debug.apk' // relative to root of project
+};
+const iosConfig = {
+  platformName: 'iOS',
+  deviceName: 'iPhone Simulator',
+  automationName: 'XCUITest',
+  app: '/Users/bentonj/Code/AppiumBugExample/ios/build/Build/Products/Debug-iphonesimulator/Lifion.app',
+};
+const driver = wd.promiseChainRemote('localhost', PORT);
+
+beforeAll(async () => {
+  await driver.init(iosConfig);
+  await driver.sleep(2000); // wait for app to load
+})
+
+test('appium renders', async () => {
+	const element = await driver.waitForElementByAccessibilityId('welcomeLabel');
+	const text = await element.text();
+	console.log(text);
+  expect(text).toEqual('Welcome to React Native!');
+});
